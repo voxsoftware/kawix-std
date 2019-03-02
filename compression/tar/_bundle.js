1badd24aaa392d23f1c173ffc4f7cf7c494ed390
James <xtreme.maicolxt94@gmail.com>
2019-03-02 07:45:02 +0000

    Fix compression/tar example and module

--- compression/tar/_bundle.js
+++ compression/tar/_bundle.js
@@ -1,13 +1,13 @@
-import Bundler from '../package/bundle.js'
+import Bundler from '../../package/bundle.js'
 import Path from 'path'
 
 var init= async function(){
     var path= Path.join(__dirname, "node_modules")
-    var outfile= Path.join(__dirname, "lowlevel.js")
+    var outfile= Path.join(__dirname, "..","tar.js")
 
     var bundler= new Bundler(path)
     bundler.virtualName= 'tar$v$4.4.8/node_modules'
-    bundler.packageJsonSupport= true
+    bundler.mainScript= "tar"
 
     await bundler.writeToFile(outfile).bundle()
 }
